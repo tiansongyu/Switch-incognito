7defbe6c78aab779905b8b6b19a88469dfdab091
invoker__qq <478880214@qq.com>
2020-10-09 21:44:46 +0800

    Create .build.sh

---
+++ .travis/.build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+sudo apt-get update -y
+sudo apt-get -y install build-essential
+
+source /etc/profile.d/devkit-env.sh
+export DEVKITA64=/opt/devkitpro/devkitA64
+
+cd /Switch-incognito && make
