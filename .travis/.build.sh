4a5e5c3b3d7a4dcca9b8dd9320672ae8e1e4b67c
invoker__qq <478880214@qq.com>
2020-10-13 15:45:32 +0800

    delete travis files

--- .travis/.build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-sudo apt-get update -y
-sudo apt-get -y install build-essential
-
-source /etc/profile.d/devkit-env.sh
-export DEVKITA64=/opt/devkitpro/devkitA64
-
-cd /Switch-incognito && make
